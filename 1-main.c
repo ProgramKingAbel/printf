7927ce783f0dd065f35b8e62d88b819f7e54674f
ProgramKingAbel <abemorara254@gmail.com>
2023-08-18 03:30:02 -0700

    test _putchar function

---
+++ 1-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _putchar('a');
+    _putchar('b');
+    _putchar('e');
+    _putchar('l');
+    _putchar('\n');
+
+    return (0);
+}
